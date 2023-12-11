eb1225a7e1f306283671fa0934a90bcb9290e336
DialloAbdou <abdoudiallo3826@yahoo.fr>
2023-12-11 16:22:02 +0100

    Initialisation Projet

---
+++ TPSolution/Debutant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Debutant
+{
+    public static Tuple<string, int, float, bool, string[]> Main()
+    {
+        // TODO
+        var name = "John"
+        var age =25
+        var weight = (float) 78.4
+        var isHuman =true
+        var friends = new string[] { "paul", "Sam", "David" };
+
+
+            return new Tuple<string, int, float, bool, string[]>
+                (name, age, weight, isHuman, friends);
+    }
+
+}
